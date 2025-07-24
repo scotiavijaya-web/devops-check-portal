3618c5c53b00119f6cc47fae2de620dc30a47d7a
vijaya <scotiavijaya@gmail.com>
2025-07-24 21:12:25 +0000

    Updated health message in conflict branch

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/health')
 def health():
-    return jsonify({"status": "UP"})
+    return jsonify({"status": "all system go"})
 
 @app.route('/metrics')
 def metrics():
